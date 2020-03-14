c04d2a46760421a8ba00642aa37b9a1a19118655
ibrahim irdem <ibrahimirdem@outlook.com>
2020-03-14 12:34:35 +0300

    Boyacı eklemeleri ve modelleri düzeltildi

--- work/boya/admin.py
+++ work/boya/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import Boyaci, Etiket
+from .models import Boyaci, Etiket, VerilenIs
 
 # Register your models here.
-admin.site.register(Boyaci)
+class BoyaciAdmin(admin.ModelAdmin):
+    list_display = ['isim', 'telefon_no', 'aciklama']
+    search_fields = ['isim', 'telefon_no', 'aciklama']
+
+    class Meta:
+        model = Boyaci
+
+class VerilenIsAdmin(admin.ModelAdmin):
+    model = VerilenIs
+    
+    list_display = ['isi_alan','is_aciklama']
+
+    def isi_alan(self, obj):
+        return obj.is_alan_boyaci.isim
+    isi_alan.admin_order_field  = 'is_alan_boyaci'
+    
+
+
+admin.site.register(Boyaci, BoyaciAdmin)
 admin.site.register(Etiket)
+admin.site.register(VerilenIs, VerilenIsAdmin)
+
--- work/boya/models.py
+++ work/boya/models.py
@@ -4,10 +4,14 @@ from django.db import models
 class Etiket(models.Model):
     etiket_adi = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "Etiket"
+        verbose_name_plural = "Etiketler"
+
     def __str__(self):
         return self.etiket_adi
-    
 
+    
 class Boyaci(models.Model):
     isim = models.CharField(max_length=100)
     telefon_no = models.CharField(max_length=11)
@@ -15,7 +19,23 @@ class Boyaci(models.Model):
     aciklama = models.TextField()
     boyaci_etiket = models.ManyToManyField(Etiket, related_name="boyacilar")
 
+    class Meta:
+        verbose_name = "Boyacı"
+        verbose_name_plural = "Boyacılar"
+
     def __str__(self):
         return self.isim
 
 
+class VerilenIs(models.Model):
+    is_alan_boyaci = models.ForeignKey(Boyaci, on_delete=models.CASCADE, 
+                                        related_name="verilen_isler", verbose_name="Boyacı")
+    is_tarihi = models.DateTimeField(auto_now_add=True, verbose_name="İş Alma Tarihi")
+    is_aciklama = models.TextField(verbose_name="Açıklama")
+
+    class Meta:
+        verbose_name = "Verilen İş"
+        verbose_name_plural = "Verilen İşler"
+
+    def __str__(self):
+        return self.is_aciklama
